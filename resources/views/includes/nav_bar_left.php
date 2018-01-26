299d9fc07c4b01c767be9f4250de6853f9264c9e
lacombe <alma31>
2018-01-26 13:42:52 +0100

    nav left bar  mise en place

--- resources/views/includes/nav_bar_left.php
+++ resources/views/includes/nav_bar_left.php
@@ -1,5 +1,59 @@
 <div class="nav_bar_left">
- <div class="logo_div">
+   <div class="logo_div">
      <img class="logo_img img-responsive" src="img/logo_ogp_blanc.svg" alt="">
- </div>
+   </div>
+     <div class="content-btn">
+         <div class="navigation">
+             <ul>
+                 <li> <a href="#">LE SPOT</a></li>
+                 <li> <a href="#">LES ACTIVITES</a></li>
+                 <li> <a href="#">LA RESTAURATION</a></li>
+                 <li> <a href="#">LES EVENEMENTS</a></li>
+                 <li> <a href="#">LES TARIFS</a></li>
+                 <li> <a href="#">LA GALERIE</a></li>
+                 <li> <a href="#">CONTACT</a></li>
+                 <li class="has-sub"> <a href="#">Magazin Online</a>
+                     <ul>
+                         <li class="has-sub"> <a href="#">Cafea</a>
+                             <ul>
+                                 <li><a href="#">Pentru Acasa</a> </li>
+                                 <li><a href="#">Horeca</a></li>
+                                 <li><a href="#">Automate Cafea</a></li>
+                             </ul>
+                         </li>
+
+
+                         <li class="has-sub"> <a href="#">Aparate Preparat Cafea</a>
+                             <ul>
+                                 <li><a href="#">Acasa Birou</a></li>
+                                 <li><a href="#">Horeca</a></li>
+                                 <li><a href="#">Horeca</a></li>
+                             </ul>
+                         </li>
+
+
+                         <li class="has-sub"> <a href="#">Accesorii</a>
+                             <ul>
+                                 <li><a href="#">Submenu 1.1.1</a></li>
+                                 <li class="has-sub"><a href="#">Submenu 1.1.2</a>
+                                     <ul>
+                                         <li><a href="#">Submenu 1.1.2.1</a></li>
+                                         <li><a href="#">Submenu 1.1.2.2</a></li>
+                                     </ul>
+                                 </li>
+                             </ul>
+                         </li>
+
+
+                     </ul>
+                 </li>
+                 <li class="has-sub"> <a href="#">Transport</a>
+                     <ul>
+                         <li><a href="#">Distribuitori</a></li>
+                     </ul>
+                 </li>
+                 <li> <a href="#">Contact</a></li>
+             </ul>
+         </div>
+     </div>
 </div>
